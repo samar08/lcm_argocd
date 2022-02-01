b6f09bb755249005cffda6083d3a6b957e0f1fe4
samar08 <samarasimhareddy.mohan@gmail.com>
2022-02-01 14:19:17 +0530

    added updateimageversion

---
+++ updateImageVersion.py
@@ -0,0 +1,21 @@
+import os
+newversion=os.environ['METALLBVERSION']
+deplin=open('deployment.yaml','r')
+
+#lines=deplin.readlines()
+newcontent=""
+for line in deplin:
+    line=line.strip('\n')
+    tline=line
+    tline=tline.strip()
+    if(tline.startswith('image')):
+        tarr=tline.split(':')
+        oldversion=tarr[-1]
+        line=line.replace(oldversion,newversion)
+    newcontent+=line+"\n"
+#print(oldversion)
+deplin.close()
+deplout=open('deployment.yaml','w')
+deplout.write(newcontent)
+deplout.close()
+    
